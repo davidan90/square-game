6daf1ea31d264e99802238d1653a32d17cae477b
David Alvarez <david16an@gmail.com>
2020-10-20 10:09:46 +0200

    Updates

---
+++ precache-manifest.5699e7d79f36655c12ed459b924f9250.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "34bb8a636a1a74b90673a7588b46dfbb",
+    "url": "/square-game/index.html"
+  },
+  {
+    "revision": "02ffea0178b17bf9511c",
+    "url": "/square-game/static/css/main.73de555e.chunk.css"
+  },
+  {
+    "revision": "e59e6182e6e6160e544d",
+    "url": "/square-game/static/js/2.f40642b1.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/square-game/static/js/2.f40642b1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "02ffea0178b17bf9511c",
+    "url": "/square-game/static/js/main.509e677e.chunk.js"
+  },
+  {
+    "revision": "f6653d899f61c8051873",
+    "url": "/square-game/static/js/runtime-main.ea7fefb6.js"
+  }
+]);
